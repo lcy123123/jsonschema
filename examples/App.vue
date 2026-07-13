fd85b2b972242b5c5053b5cd93fd2cb823e41a92
liucongyu <liucongyu>
2026-07-13 15:54:50 +0800

    删除测试git

--- examples/App.vue
+++ examples/App.vue
@@ -159,7 +159,6 @@ handleImportJson() {
     // 敏感信息方法
     handleSensitive(value) {
       console.log(value, "schema, parent11");
-      console.log('测试git')
       // 示例：弹出对话框，修改当前节点的 title 和 remark
       // this.$dialog.confirm({
       //   title: `编辑敏感字段：${value.key}`,
